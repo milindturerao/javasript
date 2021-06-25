700634d72d723e90ca617f8b884425a2e7d19f24
Milind Turerao <tureraomilind8055@gmail.com>
2021-06-25 17:26:47 +0530

    first commit

---
+++ BasicJavacript/app.js
@@ -0,0 +1,22 @@
+// log, info, warn, table, error
+// console.log("this is a JavaCript",`<h1>this is a JavaCript</h1>`,'this is a JavaCript');
+
+// console.log(20+30);
+
+console.info('this is a JavaCript');
+
+console.warn("dont click this");
+
+let stedent ={
+    // var   : var
+    // let   : Es5
+    // const : 509
+    name:'milind',
+    lName:'Turerao',
+    phone_No:8055812379
+}
+
+console.table(stedent);
+
+
+
---
+++ Variable/app.js
@@ -0,0 +1,8 @@
+console.log('This is a Variable')
+
+let a,b,c;
+a=1000;
+b=200;
+c=a+b;
+console.log(`this is add :${c} this is a A value:${a} this is a value B:${b}`);
+document.write(`<h1> ${c} ${a} ${b} </h1>`)
